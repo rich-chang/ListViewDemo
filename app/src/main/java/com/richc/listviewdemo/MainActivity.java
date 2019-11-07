ce5a1699380ccf95d923cc82b18c169bb1ca35f1
Rich Chang <rich_chang@outlook.com>
2019-11-07 11:07:17 +0800

    Add legacy ListView and set sample data with adapter

--- app/src/main/java/com/richc/listviewdemo/MainActivity.java
+++ app/src/main/java/com/richc/listviewdemo/MainActivity.java
@@ -3,6 +3,10 @@ package com.richc.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView lvFriend = findViewById(R.id.friendListView);
+
+        ArrayList<String> friendList = new ArrayList<String>();
+
+        friendList.add("Gloria");
+        friendList.add("Zoe");
+        friendList.add("Ashley");
+        friendList.add("Jarad");
+        friendList.add("Cindy");
+        friendList.add("Emma");
+
+        ArrayAdapter<String> friendAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendList);
+
+        lvFriend.setAdapter(friendAdapter);
     }
 }
