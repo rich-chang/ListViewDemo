96794f44285e75023bb5af922ba22099b3c50b06
Rich Chang <rich_chang@outlook.com>
2019-11-07 11:29:30 +0800

    Add click listener to make toast

--- app/src/main/java/com/richc/listviewdemo/MainActivity.java
+++ app/src/main/java/com/richc/listviewdemo/MainActivity.java
@@ -3,10 +3,16 @@ package com.richc.listviewdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
         ListView lvFriend = findViewById(R.id.friendListView);
 
-        ArrayList<String> friendList = new ArrayList<String>();
-
+        //ArrayList<String> friendList2 = new ArrayList<String>(asList("Gloria", "Zoe", "Ashley"));
+        final ArrayList<String> friendList = new ArrayList<String>();
         friendList.add("Gloria");
         friendList.add("Zoe");
         friendList.add("Ashley");
@@ -29,5 +35,18 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> friendAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, friendList);
 
         lvFriend.setAdapter(friendAdapter);
+
+        lvFriend.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                // adapterView: lvFriend variable
+                // view: item been clicked
+                // i: position of clicked item
+                Log.i("Clicked item", friendList.get(i));
+
+                Toast.makeText(adapterView.getContext(), "Hello "+friendList.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
